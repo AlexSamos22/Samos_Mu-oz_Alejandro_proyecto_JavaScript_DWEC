42c7d9262282330e57869d6598c1fe26cc65d15f
AlexSamos22 <asammun767@g.educaand.es>
2024-02-27 23:04:14 +0100

    validacion acabada

--- fuente/js/contacto.js
+++ fuente/js/contacto.js
@@ -3,17 +3,21 @@ let correo = document.getElementById("correo");
 let asunto = document.getElementById("asunto");
 let descripcion = document.getElementById("descripcion");
 let enviar = document.getElementById("enviar");
+let form = document.querySelector("form");
 // Deshabilitar el campo de correo electrónico al cargar la página
 correo.disabled = true;
 asunto.disabled = true;
 descripcion.disabled = true;
 enviar.disabled = true;
 
+form.addEventListener("submit", (evento) =>{
+    evento.preventDefault();
+});
+
 nombre_usuario.addEventListener('input', () => {
     let usuarioValor = nombre_usuario.value;
     let regexUsuario = /^[a-zA-Z0-9][a-zA-Z0-9_\-\.]*$/.test(usuarioValor);
 
-    
     if (!regexUsuario || usuarioValor === "") {
         // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
         correo.disabled = true;
@@ -21,12 +25,12 @@ nombre_usuario.addEventListener('input', () => {
         descripcion.disabled = true;
         enviar.disabled = true;
         nombre_usuario.setCustomValidity('El nombre de usuario debe contener al menos una letra y un número y solo puede tener los caracteres _ - .');
-        mostrarMensajeError(nombre_usuario, 'El nombre de usuario debe contener al menos una letra y un número y solo puede tener los caracteres _ - .');
+        mostrarMensajeError("usuario", 'El nombre de usuario debe contener al menos una letra y un número y solo puede tener los caracteres _ - .');
     } else {
         // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
         correo.disabled = false;
         nombre_usuario.setCustomValidity('');
-        quitarMensajeError(nombre_usuario);
+        quitarMensajeError("usuario");
     }
 });
 
@@ -41,12 +45,13 @@ correo.addEventListener('input', () => {
         descripcion.disabled = true;
         enviar.disabled = true;
         correo.setCustomValidity('El correo solo puede contener letras, numeros y - _ .');
-        mostrarMensajeError(correo, "El correo solo puede contener letras, numeros y - _ .")
+        mostrarMensajeError("correo", "El correo solo puede contener letras, numeros y - _ .")
     } else {
         // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
         asunto.disabled = false;
         correo.setCustomValidity('');
-        quitarMensajeError(correo)
+        quitarMensajeError("correo")
+
     }
 });
 
@@ -60,12 +65,12 @@ asunto.addEventListener('input', () => {
         descripcion.disabled = true;
         enviar.disabled = true;
         asunto.setCustomValidity('El asunto solo puede contener letras, numeros y espacios');
-        mostrarMensajeError(asunto, 'El asunto solo puede contener letras, numeros y espacios');
+        mostrarMensajeError("asunto", 'El asunto solo puede contener letras, numeros y espacios');
     } else {
         // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
         descripcion.disabled = false;
         asunto.setCustomValidity('');
-        quitarMensajeError(asunto);
+        quitarMensajeError("asunto");
     }
 });
 
@@ -76,28 +81,32 @@ descripcion.addEventListener('input', () => {
         // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
         enviar.disabled = true;
         descripcion.setCustomValidity('La descripcion solo puede contener letras, numeros, espacios y comas');
-        mostrarMensajeError(descripcion, 'La descripcion solo puede contener letras, numeros, espacios y comas')
+        mostrarMensajeError("descripcion", 'La descripcion solo puede contener letras, numeros, espacios y comas')
     } else {
         // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
         enviar.disabled = false;
         descripcion.setCustomValidity('');
-        quitarMensajeError(descripcion);
+        quitarMensajeError("descripcion");
     }
 });
 
-function mostrarMensajeError(elemento, mensaje) {
-    let mensajeError = elemento.nextElementSibling;
-    if (!mensajeError || !mensajeError.classList.contains('mensaje-error')) {
-        mensajeError = document.createElement('div');
-        mensajeError.classList.add('mensaje-error');
-        elemento.parentNode.insertBefore(mensajeError, elemento.nextSibling);
-    }
-    mensajeError.textContent = mensaje;
+enviar.addEventListener('click', (evento) => {
+    evento.preventDefault(); 
+    alert("Gracias por su mensaje, nos pondremos en contacto en breves momentos");
+    nombre_usuario.value = "";
+    correo.value = "";
+    asunto.value = "";
+    descripcion.value = "";
+});
+
+function mostrarMensajeError(campoError, mensaje) {
+   let parrafo = document.getElementById(`error_${campoError}`);
+   parrafo.classList.add('mensaje-error');
+   parrafo.textContent = mensaje;
 }
 
-function quitarMensajeError(elemento) {
-    let mensajeError = elemento.nextElementSibling;
-    if (mensajeError && mensajeError.classList.contains('mensaje-error')) {
-        mensajeError.remove();
-    }
+function quitarMensajeError(campoError) {
+    let parrafo = document.getElementById(`error_${campoError}`);
+    parrafo.classList.remove('mensaje-error');
+    parrafo.textContent = "";
 }
--- fuente/js/login.js
+++ fuente/js/login.js
@@ -4,29 +4,43 @@ let login = document.getElementById('login');
 let abrir_login = document.getElementById('abrir_login');
 let iniciar_session = document.getElementById('inciar_sesion');
 let registrarse = document.getElementById('registarse');
-
+let usuarioInput = document.getElementById('usuario');
+let correoInput = document.getElementById('correo');
+let claveInput = document.getElementById('Clave');
+let nombreInput = document.getElementById('Nombre');
+let apellidosInput = document.getElementById('apellidos');
+let telefonoInput = document.getElementById('telefono');
+let edadInput = document.getElementById('edad');
+let dniInput = document.getElementById('dni');
 const url_usuarios = 'https://fakestoreapi.com/users';
 
-
+correoInput.disabled = true;
+claveInput.disabled = true;
+nombreInput.disabled = true;
+apellidosInput.disabled = true;
+telefonoInput.disabled = true;
+edadInput.disabled = true;
+dniInput.disabled = true;
+registrarse.disabled = true;
 
 async function obtener_usuarios_api_y_comprobar(url_usuarios, nombre, clave) {
-   try {
-     let respuesta = await fetch(url_usuarios);
-     if (!respuesta.ok){
-        throw new Error('Error al obtener usuarios')
-     }
+    try {
+        let respuesta = await fetch(url_usuarios);
+        if (!respuesta.ok) {
+            throw new Error('Error al obtener usuarios')
+        }
 
-     let json = await respuesta.json();
-     for (let i = 0; i < json.length; i++) {
-        if (json[i].username == nombre && json[i].password == clave) {
-            return true;
-        }  
-    }
+        let json = await respuesta.json();
+        for (let i = 0; i < json.length; i++) {
+            if (json[i].username == nombre && json[i].password == clave) {
+                return true;
+            }
+        }
 
-    return false;
-   } catch (error) {
+        return false;
+    } catch (error) {
         console.error('Error al obtener usuarios:', error);
-   }
+    }
 };
 
 function comprobarUsuario_local(nombreUsuario, contraseña) {
@@ -43,7 +57,7 @@ function comprobarUsuario_local(nombreUsuario, contraseña) {
     }
 }
 
-iniciar_session.addEventListener('click', async (evento)=>{
+iniciar_session.addEventListener('click', async (evento) => {
     evento.preventDefault();
     let nombre_usuario = document.getElementById('nombre').value;
     let clave = document.getElementById('clave').value;
@@ -69,7 +83,7 @@ iniciar_session.addEventListener('click', async (evento)=>{
                 favoritos: listaFavoritos
             };
             localStorage.setItem('sesion_iniciada', JSON.stringify(sesionIniciada));
-            
+
             window.location.href = "../index.html";
         } else {
             alert("No se ha podido iniciar sesion");
@@ -80,16 +94,8 @@ iniciar_session.addEventListener('click', async (evento)=>{
 });
 
 
-registrarse.addEventListener('click', (evento) =>{
+registrarse.addEventListener('click', (evento) => {
     evento.preventDefault();
-    let usuarioInput = document.getElementById('usuario');
-    let correoInput = document.getElementById('correo');
-    let claveInput = document.getElementById('Clave');
-    let nombreInput = document.getElementById('Nombre');
-    let apellidosInput = document.getElementById('apellidos');
-    let telefonoInput = document.getElementById('telefono');
-    let edadInput = document.getElementById('edad');
-    let dniInput = document.getElementById('dni');
 
     let usuarios = {
         nombreUsuario: usuarioInput.value,
@@ -106,23 +112,217 @@ registrarse.addEventListener('click', (evento) =>{
         carrito: []
     };
 
-    localStorage.setItem(usuarioInput.value,  JSON.stringify(usuarios));
+    localStorage.setItem(usuarioInput.value, JSON.stringify(usuarios));
 
     alert("Usuario creado con exito");
     login.classList.remove('oculto');
     registro.classList.add('oculto');
+
+    usuarioInput.value = "";
+    correoInput.value= "";
+    claveInput.value = "";
+    nombreInput.value = "";
+    apellidosInput.value = "";
+    telefonoInput.value = "";
+    edadInput.value = "";
+    dniInput.value= "";
+    registrarse.value = "";
 });
 
 
-abrir_registro.addEventListener('click', (evento)=>{
+abrir_registro.addEventListener('click', (evento) => {
     evento.preventDefault();
     registro.classList.remove('oculto');
     login.classList.add('oculto');
 });
 
-abrir_login.addEventListener('click', (evento)=>{
+abrir_login.addEventListener('click', (evento) => {
     evento.preventDefault();
     login.classList.remove('oculto');
     registro.classList.add('oculto');
 });
 
+
+// Event Listener para el campo de Usuario
+usuarioInput.addEventListener('input', function () {
+    let usuarioValor = usuarioInput.value;
+    let regexUsuario = /^[a-zA-Z0-9][a-zA-Z0-9_\-\.]*$/.test(usuarioValor);
+
+    if (!regexUsuario || usuarioValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        correoInput.disabled = true;
+        claveInput.disabled = true;
+        nombreInput.disabled = true;
+        apellidosInput.disabled = true;
+        telefonoInput.disabled = true;
+        edadInput.disabled = true;
+        dniInput.disabled = true;
+        registrarse.disabled = true;
+        usuarioInput.setCustomValidity('El nombre de usuario debe contener al menos una letra y un número y solo puede tener los caracteres _ - .');
+        mostrarMensajeError("usuario", 'El nombre de usuario debe contener al menos una letra y un número y solo puede tener los caracteres _ - .');
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        correoInput.disabled = false;
+        usuarioInput.setCustomValidity('');
+        quitarMensajeError("usuario");
+    }
+});
+
+// Event Listener para el campo de Correo
+correoInput.addEventListener('input', function () {
+    let correoValor = correoInput.value;
+    let regexCorreo = /[a-zA-Z0-9_.-]+@[a-z]+\.[a-z]{2,3}$/.test(correoValor);
+
+    if (!regexCorreo || correoValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        claveInput.disabled = true;
+        nombreInput.disabled = true;
+        apellidosInput.disabled = true;
+        telefonoInput.disabled = true;
+        edadInput.disabled = true;
+        dniInput.disabled = true;
+        registrarse.disabled = true;
+        correoInput.setCustomValidity('El correo solo puede contener letras, numeros y - _ .');
+        mostrarMensajeError("correo", "El correo solo puede contener letras, numeros y - _ .");
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        claveInput.disabled = false;
+        correoInput.setCustomValidity('');
+        quitarMensajeError("correo");
+
+    }
+});
+
+// Event Listener para el campo de Clave
+claveInput.addEventListener('input', function () {
+    let claveValor = claveInput.value;
+    let regexClave = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_\-.@])[A-Za-z\d_\-.@]*/.test(claveValor);
+
+    if (!regexClave || claveValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        nombreInput.disabled = true;
+        apellidosInput.disabled = true;
+        telefonoInput.disabled = true;
+        edadInput.disabled = true;
+        dniInput.disabled = true;
+        registrarse.disabled = true;
+        claveInput.setCustomValidity('La contraseña debe contener al menos una minuscula, una mayuscula, un numero y un _ , - o .');
+        mostrarMensajeError("contraseña", "La contraseña debe contener al menos una minuscula, una mayuscula, un numero y un _ , - o .");
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        nombreInput.disabled = false;
+        claveInput.setCustomValidity('');
+        quitarMensajeError("contraseña");
+    }
+});
+
+// Event Listener para el campo de Nombre
+nombreInput.addEventListener('input', function () {
+    let nombreValor = nombreInput.value;
+    let regexNombre = /^[a-zA-Z]+$/.test(nombreValor);
+
+    if (!regexNombre || nombreValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        apellidosInput.disabled = true;
+        telefonoInput.disabled = true;
+        edadInput.disabled = true;
+        dniInput.disabled = true;
+        registrarse.disabled = true;
+        nombreInput.setCustomValidity('El nombre solo puede contener letras');
+        mostrarMensajeError("Nombre", "El nombre solo puede contener letras");
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        apellidosInput.disabled = false;
+        nombreInput.setCustomValidity('');
+        quitarMensajeError("Nombre");
+    }
+});
+
+// Event Listener para el campo de Apellidos
+apellidosInput.addEventListener('input', function () {
+    let apellidosValor = apellidosInput.value;
+    let regexApellidos = /^[A-Za-z]+ [A-Za-z]+$/.test(apellidosValor);
+
+    if (!regexApellidos || apellidosValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        telefonoInput.disabled = true;
+        edadInput.disabled = true;
+        dniInput.disabled = true;
+        registrarse.disabled = true;
+        apellidosInput.setCustomValidity('Los apellidos deben contener solo letras y estar separados por un espacio');
+        mostrarMensajeError("Apellidos", "Los apellidos deben contener solo letras y estar separados por un espacio");
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        telefonoInput.disabled = false;
+        apellidosInput.setCustomValidity('');
+        quitarMensajeError("Apellidos");
+    }
+});
+
+// Event Listener para el campo de Teléfono
+telefonoInput.addEventListener('input', function () {
+    let telfValor = telefonoInput.value;
+    let regexTelf = /^\d{9}$/.test(telfValor);
+
+    if (!regexTelf || telfValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        edadInput.disabled = true;
+        dniInput.disabled = true;
+        registrarse.disabled = true;
+        telefonoInput.setCustomValidity('Telefono no valido');
+        mostrarMensajeError("Telf", "Telefono no valido");
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        edadInput.disabled = false;
+        telefonoInput.setCustomValidity('');
+        quitarMensajeError("Telf");
+    }
+});
+
+// Event Listener para el campo de Edad
+edadInput.addEventListener('input', function () {
+    let edadfValor = edadInput.value;
+
+    if (edadfValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        dniInput.disabled = true;
+        registrarse.disabled = true;
+        edadInput.setCustomValidity('La edad no puede estar vacia');
+        mostrarMensajeError("Edad", "La edad no puede estar vacia");
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        dniInput.disabled = false;
+        edadInput.setCustomValidity('');
+        quitarMensajeError("Edad");
+    }
+});
+
+// Event Listener para el campo de DNI
+dniInput.addEventListener('input', function () {
+    let dniValor = dniInput.value;
+    let regexDNI = /^\d{8}[a-zA-Z]$/.test(dniValor);
+
+    if (!regexDNI || dniValor === "") {
+        // Si el nombre de usuario no es válido o está vacío, deshabilitar el campo de correo electrónico
+        registrarse.disabled = true;
+        telefonoInput.setCustomValidity('Formato de DNI no valido');
+        mostrarMensajeError("DNI", "Formato de DNI no valido");
+    } else {
+        // Si el nombre de usuario es válido, habilitar el campo de correo electrónico
+        registrarse.disabled = false;
+        telefonoInput.setCustomValidity('');
+        quitarMensajeError("DNI");
+    }
+});
+
+function mostrarMensajeError(campoError, mensaje) {
+    let parrafo = document.getElementById(`error_${campoError}`);
+    parrafo.classList.add('mensaje-error');
+    parrafo.textContent = mensaje;
+}
+
+function quitarMensajeError(campoError) {
+    let parrafo = document.getElementById(`error_${campoError}`);
+    parrafo.classList.remove('mensaje-error');
+    parrafo.textContent = "";
+}
